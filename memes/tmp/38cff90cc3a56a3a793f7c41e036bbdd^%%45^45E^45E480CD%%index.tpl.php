fedccf36e19e44c836dcb484fbab0fdae2c7a844
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2013-06-09 10:38:33 +0900

    First Commit

---
+++ memes/tmp/38cff90cc3a56a3a793f7c41e036bbdd^%%45^45E^45E480CD%%index.tpl.php
@@ -0,0 +1,27 @@
+<?php /* Smarty version 2.6.26, created on 2013-06-09 09:36:49
+         compiled from index.tpl */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="<?php echo $this->_tpl_vars['config']['url']; ?>
+css/ethna.css" type="text/css" />
+</head>
+<body>
+
+<div id="header">
+    <h1>Memes</h1>
+</div>
+
+<div id="main">
+    <h2>Index Page</h2>
+    <p>hello, world!</p>
+</div>
+
+<div id="footer">
+    Powered By <a href="http://ethna.jp">Ethna</a>-<?php echo @ETHNA_VERSION; ?>
+.
+</div>
+
+</body>
+</html>
